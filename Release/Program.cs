0e8d52a23abf2e47f4c4d14fc0e7856fa12f8259
Your Name <you@example.com>
2024-01-22 22:43:45 +0300

    Try to find elements in array

--- Release/Program.cs
+++ Release/Program.cs
@@ -1,2 +1,33 @@
 ﻿
-string[] words = ["Hey", "NewYear", "Best", "Battery", "Car", "System", "HardRock"];
+
+string[] words = {"Hey", "NewYear", "Best", "Battery", "Car", "System", "HardRock"};
+
+
+void  FindLenght(string[] massiv )
+
+{
+    int lenght = massiv.Length;
+
+    for(int i = 0; i < lenght; i++)
+    {
+        if(massiv[i].Length > 3)
+            {
+            System.Console.WriteLine(massiv[i]);
+            }
+
+    }
+}
+FindLenght(words);
+// System.Console.WriteLine(words.Count());
+// System.Console.WriteLine(words[0].Length);
+
+// int lenght = words.Length;
+
+//     for(int i = 0; i < lenght; i++)
+//     {
+//         if(words[i].Count() > 3)
+//             {
+//             System.Console.WriteLine(words[i]);
+//             }
+
+//     }
